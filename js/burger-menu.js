c446af18b70591c074d458cb3151e623ee26d705
Aldona Tomzik <a.tomzik@gmail.com>
2021-06-21 18:10:49 +0200

    add closing mobile menu after click on menu link

--- js/burger-menu.js
+++ js/burger-menu.js
@@ -1,6 +1,16 @@
 const btnBurger = document.getElementById("btn--burger");
 const headerNav = document.getElementById("header-nav");
-const handleClick = () => {
+
+const toggleMenu = () => {
   headerNav.classList.toggle("active");
 };
-btnBurger.addEventListener("click", handleClick);
+btnBurger.addEventListener("click", toggleMenu);
+
+//close navigation on click
+const navLinks = document.querySelectorAll(".header-nav__link");
+
+const closeMenu = () => {
+  headerNav.classList.remove("active");
+};
+
+navLinks.forEach((link) => link.addEventListener("click", closeMenu));
